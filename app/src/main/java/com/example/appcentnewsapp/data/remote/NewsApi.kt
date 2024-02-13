a32278fd6d54f15759aeea6b04ebce89dea7a578
E. Gökçen Solmaz <gokcensolmaz99@gmail.com>
2024-02-13 13:44:33 +0300

    First Commit

---
+++ app/src/main/java/com/example/appcentnewsapp/data/remote/NewsApi.kt
@@ -0,0 +1,11 @@
+package com.example.appcentnewsapp.data.remote
+
+interface NewsApi {
+
+
+
+    companion object{
+        const val BASE_URL = "https://newsapi.org/v2/"
+        const val  API_KEY = "e8a1ecdbafdd4f5da079ed98d3d0dc72"
+    }
+}
